68a6b0a631eec53bd0fd1026c4fe7adebe8d4b29
Trent Seaman <63005587+Tridentinus@users.noreply.github.com>
2026-08-19 20:16:10 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // cannot profit on one day
+        if (prices.size() < 2) return 0;
+        // track best margin 
+        int best = 0;
+        int L =0;
+        while (L < prices.size()-1) {
+            int buy = prices[L];
+            int R = L+1;
+            while (R < prices.size() && prices[R] >= buy ) {
+                best = max(best, prices[R]-buy);
+                R++;
+            }
+            if (R == prices.size()) return best;
+            L = R;
+        }
+
+
+        return best;
+
+
+
+    }
+};
