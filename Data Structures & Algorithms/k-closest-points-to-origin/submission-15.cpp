9ffeab3f2fb3a6ba9a6c5e77fe4c1ded91604df2
Trent Seaman <63005587+Tridentinus@users.noreply.github.com>
2026-08-20 22:00:03 -0400

    Add: k-closest-points-to-origin - submission-15

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-15.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    inline int dist(const vector<int>& p) {
+        return p[0]*p[0] + p[1]*p[1];
+    }
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // quick select with naive pivot selection
+        int S = 0;
+        int E = points.size()-1;
+        int K = k;
+        vector<vector<int>> res;
+        res.reserve(k);
+        int pivot = E;
+        while (true) {
+            int L = S;
+            int pDist = dist(points[pivot]);
+            for (int R = S; R < pivot; R++) {
+                if (dist(points[R]) < pDist) {
+                    // use swap to avoid deep copy
+                    swap(points[R],points[L]);
+                    L++;
+                } 
+            }
+            // use swap to avoid deep copy
+            swap(points[pivot],points[L]);
+            if (L < k-1)  {
+                S = L+1;
+                pivot = E;
+            }
+            else if (L > k-1) {
+                pivot = L-1;
+                E = pivot;
+            }
+            else {
+                break;
+            }
+        }
+        for (int i = 0; i < K; i++) {
+            res.push_back(points[i]);
+        }
+        return res;
+       
+    }
+};
