d482a30c173d0cf9acf49a1e6dac5cfc8c2a4b90
Trent Seaman <63005587+Tridentinus@users.noreply.github.com>
2026-09-01 12:07:52 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        if (n <= 2) return n;
+        vector<int> dp;
+        dp.reserve(n);
+        dp.push_back(1);
+        dp.push_back(2);
+
+        for (int i =2; i < n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        }
+
+        return dp[n-1];
+    }
+};
